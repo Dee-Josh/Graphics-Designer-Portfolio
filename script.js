4b43c591de9249a0c0563957210044f919c8b1c6
Dee Josh <joshwebprog@gmail.com>
2023-11-26 15:12:04 +0100

    Portfolio Updated

--- script.js
+++ script.js
@@ -2,6 +2,9 @@
 
 const mobileNav = document.querySelector("nav.nav");
 const bar = document.querySelector(".fa-bars");
+const arrow = document.querySelector(".arrow");
+const heroSection = document.querySelector(".hero-section-content")
+
 
 bar.addEventListener("click", ()=>{
     bar.classList.toggle("fa-bars");
@@ -15,3 +18,19 @@ mobileNav.addEventListener("click", ()=>{
     bar.classList.toggle("fa-xmark");
     bar.classList.toggle("fa-bars");
 });
+
+function isAtTheTop(element){
+    const theContainer = element.getBoundingClientRect();
+    return(
+        theContainer.top >=0 && theContainer.left>=0 && theContainer.right<=(window.innerWidth || documentElement.clientWidth)
+    );
+}
+
+setInterval(() => {
+    if(!isAtTheTop(heroSection)){
+        arrow.style.display = "flex";
+    } else{
+        arrow.style.display = "none";
+    }
+}, 100);
+
